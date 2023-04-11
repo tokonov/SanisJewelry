cffd8ea41bcb343690e23b09f9cd66a2db0325fb
tokonov <tokonovabai28@gmail.com>
2023-04-11 15:37:08 +0600

    first-commit

---
+++ src/layots/MainLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/navbar/Navbar'
+
+const MainLayout = () => {
+  return (
+    <div>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default MainLayout
---
+++ src/routes/MainRoutes.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const MainRoutes = () => {
+  return (
+    <Routes>
+        <Route>
+            <Route/>
+        </Route>
+    </Routes>
+  )
+}
+
+export default MainRoutes
