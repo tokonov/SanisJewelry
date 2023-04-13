4b9e05a3bcdc790fb795f86d30791985c5a379fb
Omuremes01~ <megamansous@gmail.com>
2023-04-13 13:36:11 +0600

    do fix

--- src/layots/MainLayout.jsx
+++ src/layots/MainLayout.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import Navbar from '../components/navbar/Navbar'
-import Footer from '../components/footer/Footer'
-import HomePage from '../pages/homePage/HomePage'
+import React from "react";
+import Navbar from "../components/navbar/Navbar";
+import Footer from "../components/footer/Footer";
+import HomePage from "../pages/homePage/HomePage";
+import { Outlet } from "react-router";
 
 const MainLayout = () => {
   return (
     <div>
-        <Navbar/>
-        <HomePage/>
-        <Footer/>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
